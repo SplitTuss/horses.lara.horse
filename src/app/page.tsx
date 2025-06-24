4654292041ef2178e6a004231fffd9b7b6ce4e43
Lara <lolaviras@gmail.com>
2025-06-24 16:23:16 -0700

    added horse images and deleted icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,97 +2,194 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <img
-          src="https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/lovis/lovis1.jpg"
-          alt="Lovis"
-          width={180}
-          height={38}
-          className="mx-auto"
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            sei mal bisschen netter.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <h4>Meet all the horses who have taught me lessons!</h4>
+        <div>
+          <h2>Lovis</h2>
+          <img
+            src="https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/lovis/lovis1.jpg"
+            alt="Lovis' face"
+            width={180}
+            height={38}
+            className="mx-auto"
+          />
+          <img
+            src="https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/lovis/lovis2.JPG"
+            alt="Lovis' head"
+            width={180}
+            height={38}
+            className="mx-auto"
+          />
+          <img
+            src="https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/lovis/lovis3.JPG"
+            alt="Lovis at show"
+            width={180}
+            height={38}
+            className="mx-auto"
+          />
+        </div>
+        <div>
+          <h2>Oz</h2>
+          <img
+            src="https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/oz/oz1.jpg"
+            alt="Oz in the mud"
+            width={180}
+            height={38}
+            className="mx-auto"
+          />
+          <img
+            src="https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/oz/oz2.JPG"
+            alt="Oz painted"
+            width={180}
+            height={38}
+            className="mx-auto"
+          />
+        </div>
+        <div>
+          <h2>Jespers Fiasko, aka Jasko</h2>
+          <img
+            src="https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/jasko/jasko1.jpg"
+            alt="Jasko and me"
+            width={180}
+            height={38}
+            className="mx-auto"
+          />
+          <img
+            src="https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/jasko/jasko2.JPG"
+            alt="Jasko and me loping"
+            width={180}
+            height={38}
+            className="mx-auto"
+          />
+          <img
+            src="https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/jasko/jasko3.JPG"
+            alt="Jasko"
+            width={180}
+            height={38}
+            className="mx-auto"
+          />
+        </div>
+        <div>
+          <h2>Tralee</h2>
+          <img
+            src="https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/tralee/tralee1.JPG"
+            alt="Tralee and me"
+            width={180}
+            height={38}
+            className="mx-auto"
+          />
+          <img
+            src="https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/tralee/tralee2.jpg"
+            alt="Tralee and me in the field"
+            width={180}
+            height={38}
+            className="mx-auto"
+          />
+          <img
+            src="https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/tralee/tralee3.JPG"
+            alt="Tralee"
+            width={180}
+            height={38}
+            className="mx-auto"
+          />
+          <img
+            src="https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/tralee/tralee4.JPG"
+            alt="Tralee at show"
+            width={180}
+            height={38}
+            className="mx-auto"
+          />
+        </div>
+        <div>
+          <h2>Pico, aka Pixel</h2>
+          <img
+            src="https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/pico/pico1.jpg"
+            alt="Pico"
+            width={180}
+            height={38}
+            className="mx-auto"
+          />
+          <img
+            src="https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/pico/pico2.JPG"
+            alt="Pico and me walking"
+            width={180}
+            height={38}
+            className="mx-auto"
+          />
+          <img
+            src="https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/pico/pico3.jpg"
+            alt="Pico"
+            width={180}
+            height={38}
+            className="mx-auto"
+          />
+        </div>
+        <div>
+          <h2>Tiana</h2>
+          <img
+            src="https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/tiana/tiana1.jpg"
+            alt="Tiana in stall"
+            width={180}
+            height={38}
+            className="mx-auto"
+          />
+          <img
+            src="https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/tiana/tiana2.jpg"
+            alt="Tiana"
+            width={180}
+            height={38}
+            className="mx-auto"
+          />
+        </div>
+        <div>
+          <h2>Nala</h2>
+          <img
+            src="https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/nala/nala1.jpeg"
+            alt="Nala"
+            width={180}
+            height={38}
+            className="mx-auto"
+          />
+          <img
+            src="https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/nala/nala2.jpeg"
+            alt="Nala"
+            width={180}
+            height={38}
+            className="mx-auto"
+          />
+        </div>
+        <div>
+          <h2>Fuchur</h2>
+          <img
+            src="https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/fuchur/fuchur1.JPG"
+            alt="Fuchur and Jimmy"
+            width={180}
+            height={38}
+            className="mx-auto"
+          />
+          <img
+            src="https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/fuchur/fuchur2.jpg"
+            alt="Fuchur and me"
+            width={180}
+            height={38}
+            className="mx-auto"
+          />
+          <img
+            src="https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/fuchur/fuchur3.jpg"
+            alt="Fuchur and me walking"
+            width={180}
+            height={38}
+            className="mx-auto"
+          />
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      <footer className="row-start-3 items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://lara.horse"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          go to lara.horse →
         </a>
       </footer>
     </div>
