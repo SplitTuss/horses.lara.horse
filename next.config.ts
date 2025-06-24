cafe180292afb154414368d1fe909a0b90a8a6e9
Lara <lolaviras@gmail.com>
2025-06-24 12:55:42 -0700

    update next config, compatibility with gh pages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
+  reactStrictMode: false,
+  trailingSlash: true,
+  assetPrefix: isProd ? '/horses.lara.horse/' : '',
+  basePath: isProd ? '/horses.lara.horse' : '',
   output: 'export',
   images: {
     remotePatterns: [new URL('https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/**')],
