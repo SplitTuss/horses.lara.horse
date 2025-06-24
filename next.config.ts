0fab717e2f4293b69606191a9bb9a2ad107f86d8
Lara <lolaviras@gmail.com>
2025-06-24 12:59:24 -0700

    remove next image url (not needed)

--- next.config.ts
+++ next.config.ts
@@ -7,10 +7,7 @@ const nextConfig: NextConfig = {
   trailingSlash: true,
   assetPrefix: isProd ? '/horses.lara.horse/' : '',
   basePath: isProd ? '/horses.lara.horse' : '',
-  output: 'export',
-  images: {
-    remotePatterns: [new URL('https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/**')],
-  },
+  output: 'export'
 };
 
 export default nextConfig;
