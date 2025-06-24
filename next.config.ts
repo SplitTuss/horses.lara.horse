7ad14b70051bcf04faa32bfdb0169511f26c300f
Lara <lolaviras@gmail.com>
2025-06-24 11:22:13 -0700

    added s3 to next js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [new URL('https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/**')],
+  },
 };
 
 export default nextConfig;
