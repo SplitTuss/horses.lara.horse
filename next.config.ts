21b93ad2abbb80bcb4cf67a7606f63293ce25f32
Lara <lolaviras@gmail.com>
2025-06-26 13:09:32 -0700

    remove extra nextjs config when using custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from 'next';
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
-  reactStrictMode: false,
-  trailingSlash: true,
-  assetPrefix: isProd ? '/horses.lara.horse/' : '',
-  basePath: isProd ? '/horses.lara.horse' : '',
   output: 'export'
 };
 
