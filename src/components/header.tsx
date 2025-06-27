0777e888b219d60211cf060031492c4b8f69e1a7
Lara <lolaviras@gmail.com>
2025-06-26 22:44:30 -0700

    changed header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,10 @@
 export function Header() {
   return (
     <ol className="bg-purple-950 flex items-center justify-center">
-      <li className="my-10 mx-10 text-purple-900"><a href="https://lara.horse">homepage</a></li>
-      <li className="my-10 mx-10 text-purple-900" >laracraft.io</li>
-      <li className="my-10 mx-10 text-purple-900">sso-log.lara.horse</li>
-      <li className="my-10 mx-10 text-purple-900">I-GRMN.laracraft.io</li>
+      <li className="p-10 text-purple-900"><a href="https://lara.horse">homepage</a></li>
+      <li className="p-10 text-purple-900"><a href="https://laracraft.io">laracraft.io</a></li>
+      {/* <li className="p-10 text-purple-900">sso-log.lara.horse</li>
+      <li className="p-10 text-purple-900">I-GRMN.laracraft.io</li> */}
     </ol>
   );
 }
