5389e49eeb550fd2ca59b455d31d7e135be2e771
Lara <lolaviras@gmail.com>
2025-06-28 12:27:10 -0700

    changed header text color

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,8 @@
 export function Header() {
   return (
     <ol className="bg-purple-950 flex items-center justify-center">
-      <li className="p-10 text-purple-900"><a href="https://lara.horse">homepage</a></li>
-      <li className="p-10 text-purple-900"><a href="https://laracraft.io">laracraft.io</a></li>
+      <li className="p-10 text-white"><a href="https://lara.horse">lara.horse</a></li>
+      <li className="p-10 text-white"><a href="https://laracraft.io">laracraft.io</a></li>
       {/* <li className="p-10 text-purple-900">sso-log.lara.horse</li>
       <li className="p-10 text-purple-900">I-GRMN.laracraft.io</li> */}
     </ol>
