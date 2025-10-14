b5e4326801814989d06bbadc175e557d932c35fa
Lara <lolaviras@gmail.com>
2025-10-14 13:40:37 -0700

    changed styling header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,18 @@
 
 export function Header() {
   return (
-    <ol className="bg-primary flex items-center justify-center gap-6 py-4 m-8">
-      <li>
+    <ol className="bg-purple-700 items-center justify-center gap-6 py-4">
+      <li className="hidden sm:block">
         <a href="https://lara.horse">
           <a>lara.horse</a>
         </a>
       </li>
-      <li className="hidden sm:block">
+      <li className="">
         <a href="https://sso-log.lara.horse/">
           <a>game horses</a>
         </a>
       </li>
-      <li className="hidden sm:block">
+      <li>
         <a href="https://laracraft.io">
           <a>laracraft.io</a>
         </a>
