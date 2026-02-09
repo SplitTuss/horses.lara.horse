9e0032c6ff99effceb4cbee72c63273c1beaa2ba
Lara <lolaviras@gmail.com>
2026-02-09 11:43:47 -0700

    styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,8 +4,7 @@ export default function Header() {
   return (
     <div className="bg-purple-700 h-25 flex items-center justify-between">
       <img src="/favicon.ico" alt="horses.lara.horse.io logo" className="w-20 h-20 rounded-lg ml-2" />
-      {/* <div className='text-3xl text-primary'>horses.lara.horse</div> */}
-       <div className='flex justify-end gap-6 mr-6'>
+       <div className='text-lg underline flex justify-end gap-6 mr-6'>
         <Link href="https://sso-log.lara.horse/">
           <p>SSO-log</p>
         </Link>
