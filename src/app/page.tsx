25ad511d0e1031e7754572d410ad99a6d0beb92a
Lara <lolaviras@gmail.com>
2026-03-05 17:32:23 -0700

    added background images to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,27 @@ export default function Home() {
     <>
       <Header />
       <main className="flex flex-col items-center">
-        <div className="m-2 flex text-center">
-          Meet my favorite horses who each have taught me some lesson! Posted ages are from the time
-          I met them.
+        <div className="grid grid-cols-2 max-w-6xl justify-between opacity-50 rounded-xl absolute">
+          <img
+            src="https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/tralee/tralee1.JPG"
+            alt="Trallee and me"
+          />
+          <img
+            src="https://s3.us-east-1.amazonaws.com/images.horses.lara.horse/winston/Winston.jpg"
+            alt="Winston and me"
+          />
         </div>
-        <Horses />
+        <div className="text-3xl sm:text-6xl relative mt-4 sm:mt-30 opacity-70">
+          Lara`s horse history
+        </div>
+        <div className="max-w-2xl text-2xl sm:text-4xl flex text-center relative mt-2 sm:mt-35 mb-5 sm:mb-80 opacity-70">
+          This is a page about all the horses I`ve met, who have found a special place in my heart.
+          Some more than others but every single one had their very own character and with that,
+          their own lessons to teach me!
+        </div>
+        <div className="">
+          <Horses />
+        </div>
       </main>
 
       <footer className="text-center m-2">
